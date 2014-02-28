eb9fb29a6d51e093f8829630dc961ceea751f174
Pavel <picasso@picasso-kubuntu-vb.(none)>
2014-02-28 10:24:38 +0400

    fix

--- rdoc/admin.py
+++ rdoc/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 from django.contrib.admin.models import LogEntry
 from rdoc.models import Doc, DocCategory
+from cms_helper.admin import ModelTranslationMedia
+from modeltranslation.admin import TranslationAdmin
 
 class LogEntryAdmin(admin.ModelAdmin):
     list_display = ('action_time', 'user', 'action_flag', 'change_message', )
@@ -9,13 +11,13 @@ class LogEntryAdmin(admin.ModelAdmin):
 admin.site.register(LogEntry,LogEntryAdmin)
     
 
-class DocAdmin(admin.ModelAdmin):
+class DocAdmin(TranslationAdmin, ModelTranslationMedia):
     list_display = ('issue_date', 'title', 'description', )    
     list_filter = ('doc_category__title', )
     search_fields = ['title', 'description']
     fields = ['issue_date', 'title', 'doc_category','description','attachment']
     
-class DocCategoryAdmin(admin.ModelAdmin):
+class DocCategoryAdmin(TranslationAdmin, ModelTranslationMedia):
     list_display = ('title',)   
     fields = ['title',]    
 
